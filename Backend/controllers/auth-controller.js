52b613fcd1be17843382e662a1fa81126ff81d4a
Taha Mahmood <115484725+tahamahmood004@users.noreply.github.com>
2025-07-09 03:41:48 +0500

    storing user data in mongodb compass

--- Backend/controllers/auth-controller.js
+++ Backend/controllers/auth-controller.js
@@ -4,10 +4,32 @@ const home = async (req, res) => {
   res.status(200).send("Welcome to Homepage using Controller");
 };
 const register = async (req, res) => {
-    const data = req.body;
-    console.log(data);
-    
-    res.status(200).send(data.email);
+  try {
+    // store data in username from req.body using structuring method
+    // const username = req.body.username
+
+    // destructuring data from req.body
+    const { username, email, password, phone } = req.body;
+
+    //find if user email i already exist
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(409).json({ message: "Email is already exists!" });
+    }
+
+    //insert data if user email is not exist
+    else {
+      const userCreated = await User.create({
+        username,
+        email,
+        password,
+        phone,
+      });
+      res.status(201).json({ message: "User registered successfully!\n", userCreated });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 const login = async (req, res) => {
   res.status(200).send("Welcome to Login page using Controller");
