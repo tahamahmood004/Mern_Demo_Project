088adf051008db08a337f811153c8a71ccdab719
Taha Mahmood <115484725+tahamahmood004@users.noreply.github.com>
2025-07-14 09:33:46 +0500

    installed reactjs, created pages and its routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/services' element={<Services/>} />
+      <Route path='/contact' element={<Contact/>} />
+    </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
